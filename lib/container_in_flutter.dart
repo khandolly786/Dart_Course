aa6c00d1bb4ae84fd1d004215d4f08707e6612be
Darakhshan Naheed <136336115+khandolly786@users.noreply.github.com>
2025-09-24 02:08:10 +0530

    Updated

---
+++ lib/container_in_flutter.dart
@@ -0,0 +1,7 @@
+// container are used to store in one box, it has only one child
+// it is by default invisible
+// it is a widget that combines common painting, positioning, and sizing operations into one widget.
+// similar to a box for storing contents
+// also a class to store one or more widgets and position them in a tree-like structure
+// containers in flutter allows many attributes to the user for decorating its child widgets such as margin, which separates the container with other contents
+// it is a feature to decor the ui layout
