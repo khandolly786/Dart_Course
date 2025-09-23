7fb637b231e003d9462dfd9aaa754ba43614ede4
Darakhshan Naheed <136336115+khandolly786@users.noreply.github.com>
2025-09-24 01:08:54 +0530

    Create final_const.dart

---
+++ lib/final_const.dart
@@ -0,0 +1,22 @@
+//unlike variables final can't be overwrite or reassign
+//for example like roll no, aadhar no
+
+void main(){
+  print("welcome");
+
+  final name="doll";
+ // name=3;//cant override
+  final String nme;
+  //final var name2; //cant set to var cuz var can be assign again and aagain
+   //const must be set at the time of declaration
+  //const is same as final but it is compile time constant
+
+  const name3 = "dollyy"; //it is compile time constant
+  //it shoud be inline with the code
+  
+
+
+
+
+
+}
