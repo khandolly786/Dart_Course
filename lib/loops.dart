efe0a13562590a49ca148fd878f419ea13a483b1
Darakhshan Naheed <136336115+khandolly786@users.noreply.github.com>
2025-09-24 01:52:29 +0530

    Create loops.dart

---
+++ lib/loops.dart
@@ -0,0 +1,38 @@
+// loops are used to write any code at a specific amount of time
+// loops
+
+main(){
+  print("hello world");
+  print("hello world");  print("hello world");
+  print("hello world");  print("hello world");
+  print("hello world");
+  print("hello world");
+  print("hello world");
+  print("hello world");
+   // redundancy is there in the code
+
+  for(int i=0;i<10;i++){ // for loop is used to repeat the code here i is the counter
+    print("hello world");
+
+  }
+// while loop is used to repeat the code until the condition is true
+//types of while loop: do while and while
+
+//while loop implementation
+  int i=0;
+  while(i<10){
+    print("while wala hello world");
+    i++;
+  }
+// do while loop implementation
+  int j=0;
+  do{
+    print("do while wala hello world");
+    j++;
+  }while(j<10);
+
+  // difference between for and while loop is that for loop is used to repeat the code until the condition is true
+  // while loop is used to repeat the code until the condition is true
+  // for loop is used to repeat the code until the condition is true
+
+}
