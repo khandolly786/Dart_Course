e7b2e384a6a74c3b0b8530c6bee9a371b982079d
Darakhshan Naheed <136336115+khandolly786@users.noreply.github.com>
2025-10-02 21:18:21 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
 
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink),
+     //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink),
       ),
       home: const MyHomePage(title: 'Hello'),
     );
