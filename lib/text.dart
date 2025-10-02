2d4eb9b21980469bf943b54938bc2f9483f3939b
Darakhshan Naheed <136336115+khandolly786@users.noreply.github.com>
2025-10-02 21:40:06 +0530

    Create text.dart

---
+++ lib/text.dart
@@ -0,0 +1,7 @@
+//text widget displays a string of text with single style
+// the string might break across multiples or might all be displayed
+// on the same line depending on the layout constraints
+// the text widget allows to write  texts
+
+
+// center widget align its child widget to the center of the available space on the screen
