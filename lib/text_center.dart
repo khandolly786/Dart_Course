bf25dd936a6b4bfa9f33b10f5cbf7b91b026467a
Darakhshan Naheed <136336115+khandolly786@users.noreply.github.com>
2025-10-06 18:37:00 +0530

    Update text_center.dart

--- lib/text_center.dart
+++ lib/text_center.dart
@@ -2,6 +2,63 @@
 // the string might break across multiples or might all be displayed
 // on the same line depending on the layout constraints
 // the text widget allows to write  texts
-
-
 // center widget align its child widget to the center of the available space on the screen
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+
+        //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink),
+      ),
+      home: const MyHomePage(title: 'Hello'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme
+              .of(context)
+              .colorScheme
+              .inversePrimary,
+          title: Text("flutter container"),
+        ),
+      body: Container(
+          width: 500,
+          height: 800,
+          color: Colors.teal,
+          child: Text('Hello Darakhshan!!'),
+
+    ),
+    );
+  }
+}
+
