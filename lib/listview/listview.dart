a1eb15139c7a43b1b9834e355e93c355d600cf7a
Darakhshan Naheed <136336115+khandolly786@users.noreply.github.com>
2025-10-07 02:12:56 +0530

    Update listview.dart

--- lib/listview/listview.dart
+++ lib/listview/listview.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink),
@@ -38,7 +39,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
 
-
+     backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text('Hello'),
       ),
       body: ListView(
@@ -66,6 +67,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
 
-        ) );
+        )
+    );
   }
 }
