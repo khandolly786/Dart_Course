3ac1238bc84aa40bd540f169a1c28f35f1ce03a3
Darakhshan Naheed <136336115+khandolly786@users.noreply.github.com>
2025-10-07 02:23:45 +0530

    Create listviewbuilder.dart

---
+++ lib/listview/listviewbuilder.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink),
+      ),
+      home: const MyHomePage(title: 'Hello'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+var arrnames= ['dolly', 'fghhj', 'ooooo', 'kkkkk','ooooooooooooo','lllllll'];
+    return Scaffold(
+      appBar: AppBar(
+
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text('Hello'),
+      ),
+      body: ListView.builder(itemBuilder: (context, index){
+        return Text(arrnames[index], style: TextStyle(fontSize: 21, fontWeight: FontWeight.w700),);
+    },
+        itemCount: arrnames.length,
+      )
+    );
+  }
+}
