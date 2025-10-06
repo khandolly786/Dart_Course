7b854bd20cdc7084855b46187903202ce7261566
Darakhshan Naheed <136336115+khandolly786@users.noreply.github.com>
2025-10-07 02:01:20 +0530

    Create listview.dart

---
+++ lib/listview.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink),
+      ),
+      home: const MyHomePage(title: 'Hello'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+
+        title: Text('Hello'),
+      ),
+      body: ListView(
+        scrollDirection: Axis.horizontal,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text('one', style: TextStyle(fontSize: 21, fontWeight: FontWeight.w700),),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text('two', style: TextStyle(fontSize: 21, fontWeight: FontWeight.w700),),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text('three', style: TextStyle(fontSize: 21, fontWeight: FontWeight.w700),),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text('four', style: TextStyle(fontSize: 21, fontWeight: FontWeight.w700),),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text('five', style: TextStyle(fontSize: 21, fontWeight: FontWeight.w700),),
+          ),
+        ],
+
+        ) );
+  }
+}
