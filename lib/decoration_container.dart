fc7df7d2e3907a7869a2f39374a317738a6a8fc0
Darakhshan Naheed <136336115+khandolly786@users.noreply.github.com>
2025-10-08 01:33:32 +0530

    Update decoration_container.dart

--- lib/decoration_container.dart
+++ lib/decoration_container.dart
@@ -68,7 +68,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
                 ),
               ],
-              shape: BoxShape.circle
+              shape: BoxShape.circle,
             ),
           ),
         ),
