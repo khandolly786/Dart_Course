0eb0aa43b03276a4ce16910a29d511f5a4cace6b
Darakhshan Naheed <136336115+khandolly786@users.noreply.github.com>
2025-10-08 01:33:42 +0530

    Create expanded_widget.dart

---
+++ lib/expanded_widget.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink),
+      ),
+      home: const MyHomePage(title: 'Hello'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text('Hello'),
+      ),
+      body:Row(
+        children: [
+        Container(
+        width: 100,
+        height: 100,
+        color: Colors.pink,
+        ),
+          Container(
+            width: 100,
+            height: 100,
+            color: Colors.red,
+          ),
+          Container(
+            width: 100,
+            height: 100,
+            color: Colors.indigo,
+          ),
+          Expanded(
+            child: Container(
+              width: 100,
+              height: 100,
+              color: Colors.lime,
+            ),
+          ),
+
+
+        ],
+      ),
+
+    );
+  }
+}
